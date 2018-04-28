fbe360c1616068bd7f3ded9447f6cb66c0ced5a8
Dustin Anderson <dandersonsoftware@outlook.com>
2018-04-28 02:43:12 +0000

    added tests for sorting strings

--- spec/sorting_spec.rb
+++ spec/sorting_spec.rb
@@ -7,6 +7,13 @@ describe '#merge_sort' do
     expect(merge_sort(unsorted)).to eq(unsorted.sort)
     expect(unsorted).to eq(unsorted_dup)
   end
+  
+  it 'sorts an array of strings' do 
+    unsorted = ["Tommy", "Tonya", "Cain", "Abel", "Tom Cruise", "Ashley Tisdale", "Ryan"]
+    unsorted_dup = unsorted.dup
+    expect(merge_sort(unsorted)).to eq(["Abel", "Ashley Tisdale", "Cain", "Ryan", "Tom Cruise", "Tommy", "Tonya"])
+    expect(unsorted).to eq(unsorted_dup)
+  end
 end
 
 describe '#insertion_sort' do 
@@ -14,6 +21,13 @@ describe '#insertion_sort' do
     unsorted = Array.new(100) {rand(1..100)}
     unsorted_dup = unsorted.dup
     expect(insertion_sort(unsorted)).to eq(unsorted.sort)
+    expect(unsorted).to eq(unsorted_dup)
+  end
+  
+  it 'sorts an array of strings' do
+     unsorted = ["Tommy", "Tonya", "Cain", "Abel", "Tom Cruise", "Ashley Tisdale", "Ryan"]
+    unsorted_dup = unsorted.dup
+    expect(insertion_sort(unsorted)).to eq(["Abel", "Ashley Tisdale", "Cain", "Ryan", "Tom Cruise", "Tommy", "Tonya"])
     expect(unsorted).to eq(unsorted_dup)
   end
 end
